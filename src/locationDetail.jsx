692bfa81b752ba6b08e6ef02bef665a877dfcc19
Alex Beltz <beltz.a@outlook.com>
2023-02-22 15:49:58 +0800

    Add locationDetails component

---
+++ src/locationDetail.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const beachBermDeliveryDetails = {
+  GMapsLink: "",
+  address: "",
+  phone: "",
+  InstagramLink: "",
+};
+const bistroDeliveryDetails = {
+  GMapsLink: "",
+  address: "",
+  phone: "",
+  InstagramLink: "",
+};
+
+export default function locationDetail() {
+  return <div>locationDetail</div>;
+}
