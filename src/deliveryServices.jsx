acca79d4e369a6a7b1825a4446afc2785c970408
Alex Beltz <beltz.a@outlook.com>
2023-02-22 15:46:40 +0800

    Add deliveryLinks component

---
+++ src/deliveryServices.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const beachBermDeliveryLinks = {
+  GoJekLink: "",
+  GrabLink: "",
+};
+const bistroDeliveryLinks = {
+  GoJekLink: "",
+  GrabLink: "",
+};
+
+export default function deliveryServices() {
+  return <div>deliveryServices</div>;
+}
