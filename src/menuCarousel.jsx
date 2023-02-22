4371164c9eaaf0869fab1f6f417ef789bfaaf16d
Alex Beltz <beltz.a@outlook.com>
2023-02-22 18:49:01 +0800

    Refactor MenuCarousel function / remove carousel btns for responsive design

--- src/menuCarousel.jsx
+++ src/menuCarousel.jsx
@@ -52,11 +52,14 @@ export const bistroMenus = [
 // export default function MenuCarousel(menu) {
 //   const [menuItems, setMenuItems] = useState(menu); // Set initial menu items to beachBermMenus
 //   const [selectedMenuItem, setSelectedMenuItem] = useState(null); // State to keep track of the currently selected menu item
+
 export default function MenuCarousel({ menuType }) {
+  // Set initial menu items to either bistroMenus or beachBermMenus based on menuType prop
   const [menuItems, setMenuItems] = useState(
     menuType === "bistro" ? bistroMenus : beachBermMenus
-  ); // Set initial menu items to either bistroMenus or beachBermMenus based on menuType prop
-  const [selectedMenuItem, setSelectedMenuItem] = useState(null); // State to keep track of the currently selected menu item
+  );
+  // State to keep track of the currently selected menu item
+  const [selectedMenuItem, setSelectedMenuItem] = useState(null);
 
   const handleMenuItemClick = (index) => {
     setSelectedMenuItem(index);
@@ -66,24 +69,24 @@ export default function MenuCarousel({ menuType }) {
     setSelectedMenuItem(null);
   };
 
-  const handlePrevButtonClick = () => {
-    const lastIndex = menuItems.length - 1;
-    const newIndex = selectedMenuItem === 0 ? lastIndex : selectedMenuItem - 1;
-    setSelectedMenuItem(newIndex);
-  };
+  //   const handlePrevButtonClick = () => {
+  //     const lastIndex = menuItems.length - 1;
+  //     const newIndex = selectedMenuItem === 0 ? lastIndex : selectedMenuItem - 1;
+  //     setSelectedMenuItem(newIndex);
+  //   };
 
-  const handleNextButtonClick = () => {
-    const lastIndex = menuItems.length - 1;
-    const newIndex = selectedMenuItem === lastIndex ? 0 : selectedMenuItem + 1;
-    setSelectedMenuItem(newIndex);
-  };
+  //   const handleNextButtonClick = () => {
+  //     const lastIndex = menuItems.length - 1;
+  //     const newIndex = selectedMenuItem === lastIndex ? 0 : selectedMenuItem + 1;
+  //     setSelectedMenuItem(newIndex);
+  //   };
 
   return (
     <div className="carousel">
       <div className="cards-wrapper">
-        <button className="prev-button" onClick={handlePrevButtonClick}>
+        {/* <button className="prev-button" onClick={handlePrevButtonClick}>
           &#8249;
-        </button>
+        </button> */}
         {menuItems.map((menuItem, index) => (
           <div
             key={index}
@@ -100,9 +103,9 @@ export default function MenuCarousel({ menuType }) {
             </div>
           </div>
         ))}
-        <button className="next-button" onClick={handleNextButtonClick}>
+        {/* <button className="next-button" onClick={handleNextButtonClick}>
           &#8250;
-        </button>
+        </button> */}
       </div>
 
       {/* <button className="prev-button" onClick={handlePrevButtonClick}>
