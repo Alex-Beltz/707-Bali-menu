ae6b38e6f9e9cadb7723d115be113a9f964d4c28
Alex Beltz <beltz.a@outlook.com>
2023-02-23 20:39:35 +0800

    Broke project, fixed project back to a good point to try again..

--- src/menuCarousel.jsx
+++ src/menuCarousel.jsx
@@ -1,3 +1,114 @@
+// import React, { useState, useEffect } from "react";
+// import "./menuCarousel.css";
+// //Beach Berm location
+// import beachBites from "./Menus/beachBermBites.jpg";
+// import beachDrinks from "./Menus/beachBermDrinks.jpg";
+// import beachIndo from "./Menus/beachBermIndo.jpg";
+// import beachWestern from "./Menus/beachBermWestern.jpg";
+// //Bistro location
+// import bistroBites from "./Menus/bistroBites.jpg";
+// import bistroDrinks from "./Menus/bistroDrinks.jpg";
+// import bistroIndo from "./Menus/bistroIndo.jpg";
+// import bistroWestern from "./Menus/bistroWestern.jpg";
+
+// export const menus = {
+//   beachBerm: [
+//     {
+//       name: "Indonesian",
+//       menuImg: beachIndo,
+//     },
+//     {
+//       name: "Western",
+//       menuImg: beachWestern,
+//     },
+//     {
+//       name: "Drinks",
+//       menuImg: beachDrinks,
+//     },
+//     {
+//       name: "Bites",
+//       menuImg: beachBites,
+//     },
+//   ],
+//   bistro: [
+//     {
+//       name: "Indonesian",
+//       menuImg: bistroIndo,
+//     },
+//     {
+//       name: "Western",
+//       menuImg: bistroWestern,
+//     },
+//     {
+//       name: "Drinks",
+//       menuImg: bistroDrinks,
+//     },
+//     {
+//       name: "Bites",
+//       menuImg: bistroBites,
+//     },
+//   ],
+// };
+
+// export default function MenuCarousel({ menuType }) {
+//   const [menuItems, setMenuItems] = useState([]);
+//   const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+
+//   useEffect(() => {
+//     setMenuItems(menus[menuType]);
+//   }, [menuType]);
+
+//   const handleMenuItemClick = (index) => {
+//     setSelectedMenuItem(index);
+//   };
+
+//   const handleBackButtonClick = () => {
+//     setSelectedMenuItem(null);
+//   };
+
+//   return (
+//     <div className="carousel">
+//       <div className="cards-wrapper">
+//         {menuItems.map((menuItem, index) => (
+//           <div
+//             key={index}
+//             className={`card ${selectedMenuItem === index ? "selected" : ""}`}
+//             onClick={() => handleMenuItemClick(index)}
+//           >
+//             <div
+//               className="card-image"
+//               style={{ backgroundImage: `url(${menuItem.menuImg})` }}
+//             >
+//               <div className="card-overlay">
+//                 <h1>{menuItem.name}</h1>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+
+//       {selectedMenuItem !== null && (
+//         <div className="lightbox">
+//           <div className="lightbox-content">
+//             <img
+//               src={menuItems[selectedMenuItem].menuImg}
+//               alt={menuItems[selectedMenuItem].name}
+//               style={{ width: "500px", height: "700px", zIndex: "100" }}
+//             />
+//             <button
+//               className="back-button"
+//               style={{ zIndex: "100" }}
+//               onClick={handleBackButtonClick}
+//             >
+//               Back
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
 import React, { useState, useEffect } from "react";
 import "./menuCarousel.css";
 //Beach Berm location
@@ -29,6 +140,7 @@ export const beachBermMenus = [
     menuImg: beachBites,
   },
 ];
+
 export const bistroMenus = [
   {
     name: "Indonesian",
@@ -112,17 +224,11 @@ export default function MenuCarousel({ menuType }) {
   );
 }
 
-// export default function MenuCarousel({ menuType }) {
-//   const [menuItems, setMenuItems] = useState(menuType);
-//   const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+// import React, { useState } from "react";
+// import "./menuCarousel.css";
 
-//   const handleClickHomeMenuBtn = (menuType) => {
-//     if (menuType === "beachBerm") {
-//       setMenuItems(beachBermMenus);
-//     } else if (menuType === "bistro") {
-//       setMenuItems(bistroMenus);
-//     }
-//   };
+// export default function MenuCarousel({ menuItems }) {
+//   const [selectedMenuItem, setSelectedMenuItem] = useState(null);
 
 //   const handleMenuItemClick = (index) => {
 //     setSelectedMenuItem(index);
@@ -131,10 +237,6 @@ export default function MenuCarousel({ menuType }) {
 //   const handleBackButtonClick = () => {
 //     setSelectedMenuItem(null);
 //   };
-
-//   useEffect(() => {
-//     handleClickHomeMenuBtn(menuType);
-//   }, [menuType]);
 
 //   return (
 //     <div className="carousel">
@@ -156,25 +258,3 @@ export default function MenuCarousel({ menuType }) {
 //           </div>
 //         ))}
 //       </div>
-
-//       {selectedMenuItem !== null && (
-//         <div className="lightbox">
-//           <div className="lightbox-content">
-//             <img
-//               src={menuItems[selectedMenuItem].menuImg}
-//               alt={menuItems[selectedMenuItem].name}
-//               style={{ width: "500px", height: "700px", zIndex: "100" }}
-//             />
-//             <button
-//               className="back-button"
-//               style={{ zIndex: "100" }}
-//               onClick={handleBackButtonClick}
-//             >
-//               Back
-//             </button>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
