76690f587d6d9aa5ed22b699b153acc18ec79baf
Alex Beltz <beltz.a@outlook.com>
2023-03-13 17:50:00 +0800

    Final cleanup before build

--- src/locationDetails.jsx
+++ src/locationDetails.jsx
@@ -4,38 +4,40 @@ import InstagramLogo from "./logos/Instagram-logo.png";
 
 export default function LocationDetails({ locationDetails }) {
   return (
-    <div className="locationDetailsCont">
-      {/* <div className="locationDetailsHeader">
+    <div>
+      <div className="locationDetailsHeader">
         <h1>FIND US</h1>
-      </div> */}
-      <div className="locationMapCont">
-        <p className="directionsLink">
-          <a
-            href={`${locationDetails.directionsLink}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Get Directions
-          </a>
-        </p>
-        <a href={locationDetails.GMapsLink}>
-          <iframe
-            title="location-map"
-            src={`${locationDetails.GMapsLink}`}
-          ></iframe>
-        </a>
       </div>
-      <div className="locationDetailsInfo">
-        <div className="locationDetailsAddress">
-          <h3>{locationDetails.address}</h3>
+      <div className="locationDetailsCont">
+        <div className="locationMapCont">
+          <p className="directionsLink">
+            <a
+              href={`${locationDetails.directionsLink}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Get Directions
+            </a>
+          </p>
+          <a href={locationDetails.GMapsLink}>
+            <iframe
+              title="location-map"
+              src={`${locationDetails.GMapsLink}`}
+            ></iframe>
+          </a>
         </div>
-        <div className="locationDetailsContact">
-          <a href={`tel:${locationDetails.phone}`}>
-            <img src={WAlogo} alt="WhatsApp logo" />
-          </a>
-          <a href={locationDetails.InstagramLink}>
-            <img src={InstagramLogo} alt="Instagram logo" />
-          </a>
+        <div className="locationDetailsInfo">
+          <div className="locationDetailsAddress">
+            <h3>{locationDetails.address}</h3>
+          </div>
+          <div className="locationDetailsContact">
+            <a href={`tel:${locationDetails.phone}`}>
+              <img src={WAlogo} alt="WhatsApp logo" />
+            </a>
+            <a href={locationDetails.InstagramLink}>
+              <img src={InstagramLogo} alt="Instagram logo" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
